1dc3b229da73f8daf8b8bae6d0f7e319f357827c
Imran Hasan <imranphero@gmail.com>
2026-08-23 11:25:44 +0600

    Remove render.yaml

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,8 @@
 import 'dotenv/config';
 import app from './app';
 
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port $ ✅✅ http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT} ✅✅ http://localhost:${PORT}`);
 });
